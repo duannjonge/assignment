b61a0636e414b79eb7e57d3ae6bfed16e4c44359
duan spot <brandennjonge@gmail.com>
2023-06-07 21:56:32 +0300

    even function

---
+++ even.js
@@ -0,0 +1,19 @@
+function calculateEvennumber(number){
+
+    let calculateEven=number%2
+    
+    if (calculateEven===0){
+    
+        console.log("is Even")
+        
+        }else
+        {
+            console.log("Odd number found")
+        
+        }
+    return calculateEven;
+    
+                   }
+    
+    
+    calculateEvennumber(6)
