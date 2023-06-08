b4162cb965da4ff8f227907be550fb020ed1e4c3
duan spot <brandennjonge@gmail.com>
2023-06-08 03:14:42 +0300

    New folder

---
+++ evens/even.js
@@ -0,0 +1,41 @@
+function calculateEvennumber(number){
+
+    let calculateEven=number%2
+    
+    if (calculateEven===0){
+    
+        console.log("is Even")
+        
+        }else
+        {
+            console.log("Odd number found")
+        
+        }
+    return calculateEven;
+    
+                   }
+    
+    
+    calculateEvennumber(6)
+
+
+    let number = parseInt(prompt("Enter the number "))
+     function calculateOutput(number){
+  
+        result=number%2
+
+    if(result===0){
+
+        console.log("this is an even number")
+
+    }else{
+
+        console.log("this is an even number ")
+
+    }
+
+    
+
+}
+   
+calculateOutput();
